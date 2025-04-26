7639182f96952f41e373046972dc2b0c0ad799ec
jos <josmerjauriacaceria@gmail.com>
2025-04-25 19:42:00 -0500

    mvc base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace examenparcial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
